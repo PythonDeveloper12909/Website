1600ee52e403e111bf0e56797470baa94c71a6fc
PythonDeveloper <pooja.tarar@gmail.com>
2025-08-14 13:46:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-let temp=document.getElementById('textarea');
-let value=document.getElementById('value');
-let radiobutton1=document.getElementById('radio1');
-let submit=document.getElementById('submit');
-let radiobutton2=document.getElementById('radio2');
-let cel_to_fahr_calc;
-let fahr_to_cel_calc;
-submit.onclick=function(){
-    if(radiobutton1.checked){
-        
-        value.textContent=Math.round((temp.value*9/5)+32) + '°F';
-    }
-    else if(radiobutton2.checked){
-        value.textContent=Math.round((temp.value-32)*5/9) + '°C';
-    }
-   else{
-        value.textContent='Select';
-    }
-}
+const rolldice = document.getElementById("butt");
+rolldice.onclick = function () {
+  let input = document.getElementById("no");
+  let ans = document.getElementById("value");
+  let random1;
+  let random2;
+  let values = [];
+  let images = [];
+  let diceimages = document.getElementById("diceimages");
+  for (let i = 0; i < input.value; i++) {
+    random1 = Math.random() * 6;
+    random2 = Math.floor(random1) + 1;
+    values.push(random2);
+    images.push(
+      `<img src="dice${random2}no.png" alt="this is a image" width="250" height="250">`
+    );
+  }
+  if (
+    input.value != 1 &&
+    input.value != 2 &&
+    input.value != 3 &&
+    input.value != 4 &&
+    input.value != 5 &&
+    input.value != 6
+  ) {
+    ans.textContent = `Sorry,Dice has only 6 faces`;
+  } else {
+    ans.textContent = `Dice:${values.join(", ")}`;
+    diceimages.innerHTML = images.join(" ");
+  }
+};
